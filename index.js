0946b8329a7ba8c670c25488bdd795123cfcb956
某亚瑟 <i@yojigen.tech>
2020-08-19 20:02:51 +0800

    Update README.MD

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 #!/usr/bin/env node
 const fs = require('fs');
 const request = require('request')
+
 const path = 'dist/'
-let strChinaList, strGoogleHosts, strChinaWhiteList, strChinaBlackList
+
+let strChinaList, strGoogleHostsList, strChinaWhiteList, strChinaBlackList
+
+fs.mkdir(path, error => {
+})
 
 request({
     url: 'https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf',
@@ -18,16 +23,40 @@ request({
         url: 'https://raw.githubusercontent.com/googlehosts/hosts/master/hosts-files/dnsmasq.conf',
         method: 'GET'
     }, (error, response, body) => {
-        strGoogleHosts = body
-        strGoogleHosts = strGoogleHosts.replace(/#.+/g, '')
-        strGoogleHosts = strGoogleHosts.replace(/address=\/(.+)\/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/g, '[/$1/]https://dns.quad9.net/dns-query')
-        strGoogleHosts = strGoogleHosts.replace('[/localhost/]https://dns.quad9.net/dns-query', '')
-        strGoogleHosts = strGoogleHosts.replace(/([\s])[\s]*/g, '$1')
-        strGoogleHosts = strGoogleHosts.trim()
-        fs.writeFileSync(path + 'GoogleHosts', strGoogleHosts, 'UTF-8')
-        strChinaWhiteList = strChinaList + '\n' + strGoogleHosts + '\nhttps://dns.quad9.net/dns-query\nhttps://dns.cloudflare.com/dns-query\nhttps://dns.google/dns-query'
+        strGoogleHostsList = body
+        strGoogleHostsList = strGoogleHostsList.replace(/#.+/g, '')
+        strGoogleHostsList = strGoogleHostsList.replace(/address=\/(.+)\/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/g, '[/$1/]https://dns.quad9.net/dns-query')
+        strGoogleHostsList = strGoogleHostsList.replace('[/localhost/]https://dns.quad9.net/dns-query', '')
+        strGoogleHostsList = strGoogleHostsList.replace(/([\s])[\s]*/g, '$1')
+        strGoogleHostsList = strGoogleHostsList.trim()
+        fs.writeFileSync(path + 'GoogleHostsList', strGoogleHostsList, 'UTF-8')
+        strChinaWhiteList = strChinaList + '\n' + strGoogleHostsList + '\nhttps://dns.quad9.net/dns-query\nhttps://dns.cloudflare.com/dns-query\nhttps://dns.google/dns-query'
         fs.writeFileSync(path + 'ChinaWhiteList', strChinaWhiteList, 'UTF-8')
-        strChinaBlackList = strChinaList + '\n' + strGoogleHosts + '\nhttps://dns.alidns.com/dns-query'
+        strChinaBlackList = strChinaList + '\n' + strGoogleHostsList + '\nhttps://dns.alidns.com/dns-query'
         fs.writeFileSync(path + 'ChinaBlackList', strChinaBlackList, 'UTF-8')
     });
+});
+
+let strHalfLifeList
+
+request({
+    url: 'https://raw.githubusercontent.com/o0HalfLife0o/list/master/ad.txt',
+    method: 'GET'
+}, (error, response, body) => {
+    strHalfLifeList = body
+    strHalfLifeList = strHalfLifeList.replace(/![\s]Title: (.+)/g, '! Title: HalfLifeList')
+    strHalfLifeList = strHalfLifeList.trim()
+    fs.writeFileSync(path + 'HalfLifeList.txt', strHalfLifeList, 'UTF-8')
+});
+
+let strAntiAD
+
+request({
+    url: 'https://anti-ad.net/easylist.txt',
+    method: 'GET'
+}, (error, response, body) => {
+    strAntiAD = body
+    strAntiAD = strAntiAD.replace(/![\s]Title: (.+)/g, '! Title: AntiAD')
+    strAntiAD = strAntiAD.trim()
+    fs.writeFileSync(path + 'AntiAD.txt', strAntiAD, 'UTF-8')
 });
